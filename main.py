50d07a28a2d48101aadcbe127dd283a0e5104174
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2025-08-08 21:51:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import streamlit as st
+from st_autorefresh import st_autorefresh
+import modos.lectura as lectura
+import modos.desarrollo as desarrollo
+import modos.mapa as mapa
+import modos.historial as historial
+import modos.configuracion as configuracion
 
+modo = st.sidebar.radio(
+    "Selecciona modo",
+    ["Lectura", "Desarrollo", "Mapa", "Historial", "Configuración"]
+)
+
+if modo in ["Lectura", "Desarrollo"]:
+    st_autorefresh(interval=10000, key=f"refresh_{modo}")
+
+if modo == "Lectura":
+    lectura.run()
+elif modo == "Desarrollo":
+    desarrollo.run()
+elif modo == "Mapa":
+    mapa.run()
+elif modo == "Historial":
+    historial.run()
+elif modo == "Configuración":
+    configuracion.run()
