373bee0c163e2e64b132dc2fd00fd68f87100660
elieceruiz <69103366+elieceruiz@users.noreply.github.com>
2025-08-08 21:45:25 -0500

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -1,1 +1,9 @@
+import pymongo
+import streamlit as st
 
+mongo_uri = st.secrets.get("mongo_uri")
+
+@st.cache_resource
+def get_db():
+    client = pymongo.MongoClient(mongo_uri)
+    return client["tracker_app"]
